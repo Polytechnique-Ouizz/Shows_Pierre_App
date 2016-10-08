a6e1bdc2c1811b4507fec445374b2bb847ed42ef
pierregaillard <pierregaillard.pg@gmail.com>
2016-10-08 12:22:05 +0200

    application statique: modif MVC et style

--- www/js/app.js
+++ www/js/app.js
@@ -50,36 +50,28 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.shows', {
+      url: '/shows',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-shows': {
+          templateUrl: 'templates/tab-shows.html',
+          controller: 'ShowsCtrl'
         }
       }
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+    .state('tab.show-detail', {
+      url: '/shows/:showId',
+      views: {
+        'tab-shows': {
+          templateUrl: 'templates/show-detail.html',
+          controller: 'ShowDetailCtrl'
+        }
       }
-    }
-  });
+    });
+    
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+     // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
 
 });
